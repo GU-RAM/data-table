0891d10454b8ac3a7120322ab62ae5ecb8a8aa3c
guro <guramidgebuadze@gmail.com>
2021-02-10 23:23:02 +0400

    finished popup with carousel

---
+++ src/components/ImgCarousel/ImgCarousel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import cat1 from '../../assets/images/cats-1.jpg';
+import cat2 from '../../assets/images/cats-2.jpg';
+import cat3 from '../../assets/images/cats-3.jpg';
+import cat4 from '../../assets/images/cats-4.jpg';
+import cat5 from '../../assets/images/cats-5.jpg';
+import cat6 from '../../assets/images/cats-6.jpg';
+
+const ImgCarousel = () => {
+  return (
+    <>
+      <Carousel className='img-carousel'>
+        <div>
+          <img alt='cat' src={cat1} />
+        </div>
+        <div>
+          <img alt='cat' src={cat2} />
+        </div>
+        <div>
+          <img alt='cat' src={cat3} />
+        </div>
+        <div>
+          <img alt='cat' src={cat4} />
+        </div>
+        <div>
+          <img alt='cat' src={cat5} />
+        </div>
+        <div>
+          <img alt='cat' src={cat6} />
+        </div>
+      </Carousel>
+    </>
+  );
+};
+
+export default ImgCarousel;
--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -1,35 +1,42 @@
 import React from 'react';
+import ImgCarousel from '../ImgCarousel/ImgCarousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const PopUp = ({ rowInfo }) => {
+const PopUp = ({ rowInfo, setShowModal }) => {
   const { id, first_name, last_name, date_of_birth, country, phone } = rowInfo;
   return (
-    <div>
+    <div className='PopUp'>
+      <div className='PopUp-header'>
+        <h2>User Information</h2>
+        <div onClick={() => setShowModal(false)}>X</div>
+      </div>
       <ol>
         <li>
-          <strong>ID</strong>
+          <strong>ID: </strong>
           {id}
         </li>
         <li>
-          <strong>First Name</strong>
+          <strong>First Name: </strong>
           {first_name}
         </li>
         <li>
-          <strong>Last Name</strong>
+          <strong>Last Name: </strong>
           {last_name}
         </li>
         <li>
-          <strong>Date of Birth</strong>
+          <strong>Date of Birth: </strong>
           {date_of_birth}
         </li>
         <li>
-          <strong>Country</strong>
+          <strong>Country: </strong>
           {country}
         </li>
         <li>
-          <strong>Phone</strong>
+          <strong>Phone: </strong>
           {phone}
         </li>
       </ol>
+      <ImgCarousel />
     </div>
   );
 };
